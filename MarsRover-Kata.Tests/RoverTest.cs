889b22d7704e9db9735e59217cdd64ceba7e3224
Ismaïl <nguyen.ismail@gmail.com>
2017-11-15 22:13:18 +0100

    Rover land with given position and direction

--- MarsRover-Kata.Tests/RoverTest.cs
+++ MarsRover-Kata.Tests/RoverTest.cs
@@ -5,15 +5,20 @@ namespace MarsRover_Kata.Tests
 {
     public class RoverTest
     {
-        [Test]
-        public void Should()
+        [TestCase(0, 0, "N", "0,0,N")]
+        [TestCase(0, 0, "S", "0,0,S")]
+        [TestCase(0, 0, "E", "0,0,E")]
+        [TestCase(0, 0, "W", "0,0,W")]
+        public void Should_Rover_Land_Correctly(int x, int y, string direction, string expectedPosition)
         {
             // GIVEN
+            Rover rover = new Rover(x, y, direction);
 
             // WHEN
+            string roverPosition = rover.ToString();
 
             // THEN
-            Check.That(true).IsTrue();
+            Check.That(roverPosition).IsEqualTo(expectedPosition);
         }
     }
 }
--- MarsRover-Kata/Rover.cs
+++ MarsRover-Kata/Rover.cs
@@ -1,6 +1,23 @@
-﻿namespace MarsRover_Kata
+﻿using System;
+
+namespace MarsRover_Kata
 {
     public class Rover
     {
+        private int x;
+        private int y;
+        private string direction;
+
+        public Rover(int x, int y, string direction)
+        {
+            this.x = x;
+            this.y = y;
+            this.direction = direction;
+        }
+
+        public override string ToString()
+        {
+            return $"{x},{y},{direction}";
+        }
     }
 }
